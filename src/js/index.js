935fb1cd1087ce3855e5abfbf617b5cf9cd9cf55
Flor <fcflorencia@hotmail.es>
2021-01-29 22:45:00 +0000

    function play

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,11 @@ import { gameData } from './questions.js';
 
 const data = gameData.countries;
 
+const playButton = document.querySelector('button');
+playButton.addEventListener('click', () => {
+    play();
+})
+
 const random = (array) => Math.floor(Math.random() * array.length);
 
 /** @type {*} */
@@ -9,11 +14,23 @@ const random = (array) => Math.floor(Math.random() * array.length);
 let game = [];
 const nQuestions = 5;
 
-play();
 function play() {
+    cleanGameSection();
     game = getQuestions();
-    getCountries();
-    getCities();
+    cities();
+    countries();
+}
+
+function cleanGameSection() {
+    const gameDivElements = [...document.querySelectorAll('#game > div')];
+    gameDivElements.forEach(element => {
+        if (element.hasChildNodes) {
+            const children = [...element.children];
+            children.forEach(child => {
+                child.remove();
+            });
+        }
+    });
 }
 
 function getQuestions() {
@@ -29,12 +46,48 @@ function getQuestions() {
     return questions;
 }
 
-function getCountries() {
-    const countries = game.map(question => question.name);
+function cloneTemplate(array, id) {
+    const template = document.querySelector(`#${id}-template`);
+
+    const div = template.content.querySelector('div:first-child');
+    console.log(div)
+
+    const titleElement = template.content.querySelector('h3');
+
+    const clones = []
+    array.forEach(item => {
+        (id === 'cities') ? div.setAttribute('data-code', item.cityCode) : div.setAttribute('data-code', item.code);
+        titleElement.textContent = item.name;
+        const clone = document.importNode(template.content, true);
+        clones.push(clone)
+    });
+    return clones;
 }
 
-function getCities() {
-    const cities = game.map(question => question.cities[random(question.cities)]);
+function showMessyClones(array, id) {
+    const clones = array;
+    const messyClones = [];
+    while (messyClones.length < clones.length) {
+        const clone = clones[random(clones)];
+        if (!messyClones.includes(clone)) {
+            messyClones.push(clone);
+        }
+    }
+    const div = document.querySelector(`#${id}`);
+    messyClones.forEach(clone => {
+        div.appendChild(clone);
+    });
+}
+
+function cities() {
+    const cities = game.map(question => question.cities[random(question.cities)]);    
+    const clones = cloneTemplate(cities, 'cities');
+    showMessyClones(clones, 'cities');
+}
+
+function countries() {
+    const clones = cloneTemplate(game, 'countries');
+    showMessyClones(clones, 'countries');
 }
 
 $('.city').draggable();
