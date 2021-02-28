d1d8e27273bfa417a7171a9451a1c9c3fa8c7d15
Flor <fcflorencia@hotmail.es>
2021-02-28 21:55:40 +0000

    docs

--- src/js/map.js
+++ src/js/map.js
@@ -1,9 +1,16 @@
 import { gameData } from './questions.js';
 
+/** Información para la localización inicial del mapa. */
+/** @type {String} */
 const schoolName = 'CIFP César Manrique';
+/** @type {Array} */
 const schoolLocation = [28.456099086100803, -16.283138740418725];
 
+/** Inicializa el mapa de Leaflet
+ * y establece su localización.
+*/
 const mymap = L.map('map-section').setView(schoolLocation, 15);
+// Añade la capa, el mapa a mostrar, usando OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     minZoom: 1,
@@ -11,11 +18,19 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     id: 'leaflet-osm'
 }).addTo(mymap);
 
+/** Añade un marcador. */
 let marker = L.marker(schoolLocation).addTo(mymap)
-.bindPopup(schoolName)
-.openPopup();
+    .bindPopup(schoolName)
+    .openPopup();
 
-function map(name = schoolName, location = schoolLocation) {
+/**
+ * @description Cambia la posición del mapa con un efecto vuelo.
+ * Elimina la marca anterior y añade otra.
+ * @author Florencia Del Castillo Fleitas
+ * @param {String} [name=schoolName]
+ * @param {Array} [location=schoolLocation]
+ */
+function map (name = schoolName, location = schoolLocation) {
     mymap.flyTo(location, 15);
     marker.remove();
     marker = L.marker(location).addTo(mymap)
@@ -23,6 +38,13 @@ function map(name = schoolName, location = schoolLocation) {
         .openPopup();
 }
 
+/**
+ * @description Obtiene las coordenadas de una ciudad.
+ * @author Florencia Del Castillo Fleitas
+ * @param {String} countryCode
+ * @param {String} cityName
+ * @return {Array} Coordenadas.
+ */
 const getCityLocation = (countryCode, cityName) => {
     return gameData.countries.filter(country => {
         return country.code === countryCode;
@@ -35,7 +57,13 @@ const getCityLocation = (countryCode, cityName) => {
     });
 };
 
-function moveMap(cityElement) {
+/**
+ * @description Actualiza el mapa cuando se ha relacionado
+ * una ciudad con el país que le corresponde.
+ * @author Florencia Del Castillo Fleitas
+ * @param {HTMLDivElement} cityElement
+ */
+function moveMap (cityElement) {
     const countryCode = cityElement.dataset.code;
     const cityName = cityElement.firstElementChild.textContent;
     const location = getCityLocation(countryCode, cityName);
@@ -46,3 +74,4 @@ export {
     map,
     moveMap
 }
+;
